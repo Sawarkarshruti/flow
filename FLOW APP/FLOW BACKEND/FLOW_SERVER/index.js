bd6e400c042316315b5821b55b4e2a3f8a90531a
Sawarkarshruti <shrutikasawarkar16@gmail.com>
2025-08-01 22:35:27 +0530

    Created index.js for loading express app in port 3000

---
+++ FLOW APP/FLOW BACKEND/FLOW_SERVER/index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const userRouter = require("./route/users")
+const {jwtAuth} = require("./utils/jwtauth")
+
+app.use(express.json())
+app.use(jwtAuth)
+app.use("/users", userRouter)
+
+const port = 3000
+app.listen(port, "0.0.0.0", () => {
+    console.log("server ready at port", port)
+})
